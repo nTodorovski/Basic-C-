c638cae64e9d9b0b0935c7d11477cf71d083d663
Nikola Todorovski <todorovski_nikola@hotmail.com>
2019-02-25 13:33:34 +0100

    Thread.Sleep and Console.ForegroundColor

--- MyFirstConsoleApp/Stars/Program.cs
+++ MyFirstConsoleApp/Stars/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Stars
 {
@@ -10,11 +10,28 @@ namespace Stars
     {
         static void Main(string[] args)
         {
+            //var oldColor = Console.ForegroundColor;
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine($"Hello World in {Console.ForegroundColor}!");
+            //Thread.Sleep(2000);
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.WriteLine($"Hello World in {Console.ForegroundColor}!");
+            //Thread.Sleep(2000);
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine($"Hello World in {Console.ForegroundColor}!");
+            //Console.ForegroundColor = oldColor;
+            //Console.ReadLine();
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Christmas Tree in {Console.ForegroundColor}!");
+            Thread.Sleep(2000);
             Console.WriteLine("    *");
             Console.WriteLine("   ***");
             Console.WriteLine("  *****");
             Console.WriteLine(" *******");
             Console.WriteLine("*********");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
             Console.ReadLine();
         }
     }
