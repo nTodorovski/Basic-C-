eb4a9a8970fab99fbc66a02a9a09f4f91dbb597c
Nikola Todorovski <nTodorovski@users.noreply.github.com>
2019-03-04 16:57:26 +0100

    new exercises

---
+++ Exercises/Divide the product composed of their digits/Solution1/Task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                string number = i.ToString();
+                int number1 = i;
+                int sum = 0;
+                for (int j = 0; j < number.Length; j++)
+                {
+                    sum = sum + (number1 % 10);
+                    number1 = number1 / 10;
+                }
+                if(i % sum == 0)
+                {
+                    Console.WriteLine($"{i} is dividable with the product of his digits.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercises/Yes or No/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            int index = 0;
+            string[] names = new string[1];
+            string yesNo = "";
+
+            do
+            {
+                Console.WriteLine("Enter name:");
+                var input = Console.ReadLine();
+                names[index] = input;
+
+                foreach (string name in names)
+                {
+                    Console.Write($"{name} ");
+                    Thread.Sleep(100);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Do you want to enter new name? Y/N");
+                yesNo = Console.ReadLine();
+                while (yesNo != "Y" && yesNo != "N")
+                {
+                    Console.WriteLine("Enter valid input");
+                    yesNo = Console.ReadLine();
+                }
+                if (yesNo == "Y")
+                {
+                    counter++;
+                    index++;
+                    Array.Resize(ref names, counter);
+                }
+            } while (yesNo != "N");
+            Console.ReadLine();
+        }
+    }
+}
