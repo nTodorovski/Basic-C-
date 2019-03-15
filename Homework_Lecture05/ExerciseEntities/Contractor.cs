91e79025022741f5bfb137877f19d8da271d88ee
Nikola Todorovski <nTodorovski@users.noreply.github.com>
2019-03-16 00:25:10 +0100

    Homework Lecture 5

---
+++ Homework_Lecture05/ExerciseEntities/Contractor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseEntities
+{
+    public class Contractor : Employee
+    {
+        public double WorkHours { get; set; }
+        public int PayPerHour { get; set; }
+        public Manager Responsible { get; set; }
+
+        public Contractor(string firstName,string lastName, double workHours,int payPerHour,Manager responsible)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            WorkHours = workHours;
+            PayPerHour = payPerHour;
+            Responsible = responsible;
+            Salary = 800;
+            Role = Role.Contractor;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine("First Name: {0}, Last Name: {1}, Salary: {2} Role: {3}, Working under Manager: {4} {5}", FirstName, LastName, GetSalary(), Role, Responsible.FirstName, Responsible.LastName);
+        }
+
+        public override double GetSalary()
+        {
+            Salary = WorkHours * PayPerHour;
+            return WorkHours * PayPerHour; ;
+        }
+
+        public Manager CurrentPosition()
+        {
+            return Responsible;
+        }
+    }
+}
